9d566abd51e020d108793e0c8f5028dd74bb9afe
Murat Özcan <102511241+muratozcan8@users.noreply.github.com>
2024-07-29 16:47:13 +0300

    added endpoints to movie api service about movie informations

--- app/src/main/java/com/obss/firstapp/network/MovieApiService.kt
+++ app/src/main/java/com/obss/firstapp/network/MovieApiService.kt
@@ -1,8 +1,13 @@
 package com.obss.firstapp.network
 
+import com.obss.firstapp.model.actor.Actor
+import com.obss.firstapp.model.actor.ActorMovies
+import com.obss.firstapp.model.credit.Credits
 import com.obss.firstapp.model.movie.Movie
 import com.obss.firstapp.model.movie.MovieList
 import com.obss.firstapp.model.movieDetail.MovieDetail
+import com.obss.firstapp.model.review.Review
+import com.obss.firstapp.model.video.Video
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -10,18 +15,44 @@ import retrofit2.http.Query
 interface MovieApiService {
 
     @GET("movie/popular")
-    suspend fun getPopularMovies(@Query("page") page: Int): MovieList
+    suspend fun getPopularMovies(
+        @Query("page") page: Int
+    ): MovieList
 
     @GET("movie/top_rated")
-    suspend fun getTopRatedMovies(@Query("page") page: Int): MovieList
+    suspend fun getTopRatedMovies(
+        @Query("page") page: Int
+    ): MovieList
 
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(
-        @Path("movie_id") movieId: Int,
+        @Path("movie_id") movieId: Int
     ): MovieDetail
 
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id") movieId: Int
+    ): Credits
 
+    @GET("person/{person_id}")
+    suspend fun getActorDetails(
+        @Path("person_id") personId: Int,
+    ): Actor
 
+    @GET("person/{person_id}/movie_credits")
+    suspend fun getActorMovies(
+        @Path("person_id") personId: Int,
+    ): ActorMovies
+
+    @GET("movie/{movie_id}/reviews")
+    suspend fun getMovieReviews(
+        @Path("movie_id") movieId: Int,
+    ): Review
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieVideos(
+        @Path("movie_id") movieId: Int,
+    ): Video
 
 
 
