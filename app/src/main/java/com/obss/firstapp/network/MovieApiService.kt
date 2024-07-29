66fffd4f81a44588aa584c3d046cfd96ef48ef1c
Murat Özcan <102511241+muratozcan8@users.noreply.github.com>
2024-07-29 14:53:17 +0300

    created Movie API service

---
+++ app/src/main/java/com/obss/firstapp/network/MovieApiService.kt
@@ -0,0 +1,25 @@
+package com.obss.firstapp.network
+
+import com.obss.firstapp.model.movie.MovieList
+import com.obss.firstapp.model.movieDetail.MovieDetail
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApiService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("page") page: Int): MovieList
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(): MovieList
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+    ): MovieDetail
+
+
+
+
+}
