bf1ed78924d9d95f83d5eb74cce1024763c37a99
Murat Özcan <102511241+muratozcan8@users.noreply.github.com>
2024-07-30 11:15:16 +0300

    created movie item for linear design

--- app/src/main/java/com/obss/firstapp/ui/adapter/PopularMovieAdapter.kt
+++ app/src/main/java/com/obss/firstapp/ui/adapter/PopularMovieAdapter.kt
@@ -5,18 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.obss.firstapp.databinding.MovieGridItemBinding
+import com.obss.firstapp.databinding.MovieItemLinearBinding
 import com.obss.firstapp.model.movie.Movie
+import kotlin.math.roundToInt
 
 class PopularMovieAdapter() : RecyclerView.Adapter<PopularMovieAdapter.ViewHolder>() {
 
     private var popularMovieList: List<Movie> = listOf()
 
-    inner class ViewHolder(val binding: MovieGridItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: MovieItemLinearBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = MovieGridItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = MovieItemLinearBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -24,9 +26,9 @@ class PopularMovieAdapter() : RecyclerView.Adapter<PopularMovieAdapter.ViewHolde
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = popularMovieList[position]
-        holder.binding.ivMovieGrid.load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
-        holder.binding.tvMovieGrid.text = movie.title
-        holder.binding.tvMovieScoreGrid.text = movie.voteAverage.toString()
+        holder.binding.ivMovieLinear.load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
+        holder.binding.tvMovieLinear.text = movie.title
+        holder.binding.tvMovieScoreLinear.text = (((movie.voteAverage?.times(10))?.roundToInt() ?: 0) / 10.0).toString()
     }
 
     fun updateList(newList: List<Movie>) {
--- app/src/main/java/com/obss/firstapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/obss/firstapp/ui/home/HomeViewModel.kt
@@ -23,7 +23,7 @@ class HomeViewModel @Inject constructor(private val movieApi: MovieApiService): 
 
     fun getPopularMovies() {
         viewModelScope.launch {
-            val response = movieApi.getPopularMovies(2)
+            val response = movieApi.getPopularMovies(1)
             _popularMovieList.update { response }
         }
     }
