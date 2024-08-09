14943a206893c3123e0d5f314c87cd4aef479dc0
Murat Özcan <102511241+muratozcan8@users.noreply.github.com>
2024-08-09 09:49:49 +0300

    fixed bug on search fragment about landscape mode

--- app/src/main/java/com/obss/firstapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/obss/firstapp/ui/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.obss.firstapp.ui.search
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.obss.firstapp.databinding.FragmentSearchBinding
 import com.obss.firstapp.ext.collectFlow
 import com.obss.firstapp.model.movieSearch.MovieSearch
@@ -37,6 +39,7 @@ class SearchFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         changeVisibilityBottomBar(true)
+        changeSpanCount()
         searchMovie()
         setSearchMovieListAdapter()
         setLoadingProgressBar()
@@ -83,13 +86,26 @@ class SearchFragment : Fragment() {
         (activity as MainActivity).changeVisibilityBottomBar(isVisible)
     }
 
+    private fun changeSpanCount() {
+        val isLandscape =
+            resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+        SPAN_COUNT_SEARCH = if (isLandscape) SPAN_COUNT_LANDSCAPE_GRID_SEARCH else SPAN_COUNT_GRID_SEARCH
+    }
+
     private fun initRecyclerAdapter(searchedMovieList: List<MovieSearch>) {
         val adapter = SearchMovieAdapter()
         binding.rvSearchMovie.adapter = adapter
+        binding.rvSearchMovie.layoutManager = GridLayoutManager(context, SPAN_COUNT_SEARCH)
         adapter.updateList(searchedMovieList)
         adapter.setOnItemClickListener { movie ->
             val direction = SearchFragmentDirections.actionSearchFragmentToDetailFragment(movie.id!!)
             findNavController().navigate(direction)
         }
+    }
+
+    companion object {
+        private var SPAN_COUNT_SEARCH = 3
+        private var SPAN_COUNT_GRID_SEARCH = 3
+        private var SPAN_COUNT_LANDSCAPE_GRID_SEARCH = 6
     }
 }
